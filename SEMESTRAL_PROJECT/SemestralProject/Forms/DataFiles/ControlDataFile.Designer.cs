80e3bb8f034f8a595d4155ecb4f2b8979d210f50
Jiří Škoda <jiri.skoda@student.upce.cz>
2023-04-20 12:33:00 +0200

    Data files GUI implementation started

---
+++ SEMESTRAL_PROJECT/SemestralProject/Forms/DataFiles/ControlDataFile.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace SemestralProject.Forms.DataFiles
+{
+    partial class ControlDataFile
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanelContent = new System.Windows.Forms.FlowLayoutPanel();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.tableLayoutPanelPath = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.panelPathBrowse = new System.Windows.Forms.Panel();
+            this.labelSpace1 = new System.Windows.Forms.Label();
+            this.labelInformationSystem = new System.Windows.Forms.Label();
+            this.comboBoxInformationSystem = new System.Windows.Forms.ComboBox();
+            this.labelMap = new System.Windows.Forms.Label();
+            this.comboBoxMap = new System.Windows.Forms.ComboBox();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanelContent.SuspendLayout();
+            this.tableLayoutPanelPath.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanelContent
+            // 
+            this.flowLayoutPanelContent.Controls.Add(this.labelPath);
+            this.flowLayoutPanelContent.Controls.Add(this.tableLayoutPanelPath);
+            this.flowLayoutPanelContent.Controls.Add(this.labelSpace1);
+            this.flowLayoutPanelContent.Controls.Add(this.labelInformationSystem);
+            this.flowLayoutPanelContent.Controls.Add(this.comboBoxInformationSystem);
+            this.flowLayoutPanelContent.Controls.Add(this.labelMap);
+            this.flowLayoutPanelContent.Controls.Add(this.comboBoxMap);
+            this.flowLayoutPanelContent.Controls.Add(this.labelDescription);
+            this.flowLayoutPanelContent.Controls.Add(this.textBoxDescription);
+            this.flowLayoutPanelContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanelContent.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanelContent.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanelContent.Name = "flowLayoutPanelContent";
+            this.flowLayoutPanelContent.Size = new System.Drawing.Size(552, 411);
+            this.flowLayoutPanelContent.TabIndex = 0;
+            // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(3, 0);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(114, 20);
+            this.labelPath.TabIndex = 0;
+            this.labelPath.Text = "Cesta k souboru";
+            // 
+            // tableLayoutPanelPath
+            // 
+            this.tableLayoutPanelPath.ColumnCount = 2;
+            this.tableLayoutPanelPath.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 72.89378F));
+            this.tableLayoutPanelPath.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.10623F));
+            this.tableLayoutPanelPath.Controls.Add(this.panelPathBrowse, 1, 0);
+            this.tableLayoutPanelPath.Controls.Add(this.textBoxPath, 0, 0);
+            this.tableLayoutPanelPath.Location = new System.Drawing.Point(3, 23);
+            this.tableLayoutPanelPath.Name = "tableLayoutPanelPath";
+            this.tableLayoutPanelPath.RowCount = 1;
+            this.tableLayoutPanelPath.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelPath.Size = new System.Drawing.Size(546, 44);
+            this.tableLayoutPanelPath.TabIndex = 1;
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(3, 3);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(392, 27);
+            this.textBoxPath.TabIndex = 0;
+            // 
+            // panelPathBrowse
+            // 
+            this.panelPathBrowse.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelPathBrowse.Location = new System.Drawing.Point(401, 3);
+            this.panelPathBrowse.Name = "panelPathBrowse";
+            this.panelPathBrowse.Size = new System.Drawing.Size(142, 38);
+            this.panelPathBrowse.TabIndex = 1;
+            // 
+            // labelSpace1
+            // 
+            this.labelSpace1.AutoSize = true;
+            this.labelSpace1.Location = new System.Drawing.Point(3, 70);
+            this.labelSpace1.Name = "labelSpace1";
+            this.labelSpace1.Size = new System.Drawing.Size(13, 20);
+            this.labelSpace1.TabIndex = 2;
+            this.labelSpace1.Text = " ";
+            // 
+            // labelInformationSystem
+            // 
+            this.labelInformationSystem.AutoSize = true;
+            this.labelInformationSystem.Location = new System.Drawing.Point(3, 90);
+            this.labelInformationSystem.Name = "labelInformationSystem";
+            this.labelInformationSystem.Size = new System.Drawing.Size(129, 20);
+            this.labelInformationSystem.TabIndex = 3;
+            this.labelInformationSystem.Text = "Informační systém";
+            // 
+            // comboBoxInformationSystem
+            // 
+            this.comboBoxInformationSystem.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxInformationSystem.FormattingEnabled = true;
+            this.comboBoxInformationSystem.Location = new System.Drawing.Point(3, 113);
+            this.comboBoxInformationSystem.Name = "comboBoxInformationSystem";
+            this.comboBoxInformationSystem.Size = new System.Drawing.Size(543, 28);
+            this.comboBoxInformationSystem.TabIndex = 4;
+            // 
+            // labelMap
+            // 
+            this.labelMap.AutoSize = true;
+            this.labelMap.Location = new System.Drawing.Point(3, 167);
+            this.labelMap.Name = "labelMap";
+            this.labelMap.Size = new System.Drawing.Size(52, 20);
+            this.labelMap.TabIndex = 5;
+            this.labelMap.Text = "Oblast";
+            // 
+            // comboBoxMap
+            // 
+            this.comboBoxMap.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMap.FormattingEnabled = true;
+            this.comboBoxMap.Location = new System.Drawing.Point(3, 190);
+            this.comboBoxMap.Name = "comboBoxMap";
+            this.comboBoxMap.Size = new System.Drawing.Size(543, 28);
+            this.comboBoxMap.TabIndex = 6;
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Location = new System.Drawing.Point(3, 241);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(44, 20);
+            this.labelDescription.TabIndex = 7;
+            this.labelDescription.Text = "Popis";
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(3, 264);
+            this.textBoxDescription.Multiline = true;
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(543, 144);
+            this.textBoxDescription.TabIndex = 8;
+            // 
+            // ControlDataFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.flowLayoutPanelContent);
+            this.Name = "ControlDataFile";
+            this.Size = new System.Drawing.Size(552, 411);
+            this.flowLayoutPanelContent.ResumeLayout(false);
+            this.flowLayoutPanelContent.PerformLayout();
+            this.tableLayoutPanelPath.ResumeLayout(false);
+            this.tableLayoutPanelPath.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanelContent;
+        private Label labelPath;
+        private TableLayoutPanel tableLayoutPanelPath;
+        private TextBox textBoxPath;
+        private Panel panelPathBrowse;
+        private Label labelSpace1;
+        private Label labelInformationSystem;
+        private ComboBox comboBoxInformationSystem;
+        private Label labelMap;
+        private ComboBox comboBoxMap;
+        private Label labelDescription;
+        private TextBox textBoxDescription;
+    }
+}
---
+++ SEMESTRAL_PROJECT/SemestralProject/Forms/DataFiles/ControlDataFile.cs
@@ -0,0 +1,68 @@
+﻿using SemestralProject.Data;
+using SemestralProject.Utils;
+using SemestralProject.Visual;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SemestralProject.Forms.DataFiles
+{
+    /// <summary>
+    /// Class representing control for creating and editing data files
+    /// </summary>
+    internal partial class ControlDataFile : UserControl, IControl
+    {
+        /// <summary>
+        /// Structure holding all necessary information for showing all information systems in combobox
+        /// </summary>
+        private struct InformationSystemItem
+        {
+            /// <summary>
+            /// Displayed name in combo box
+            /// </summary>
+            public string DisplayName { get; set; }
+
+            /// <summary>
+            /// Information system which combobox item represents
+            /// </summary>
+            public InformationSystem System { get; set; }
+        }
+
+        /// <summary>
+        /// Wrapper of all program resources
+        /// </summary>
+        public Context Context { get; init; }
+
+        /// <summary>
+        /// Creates new control for creating data files
+        /// </summary>
+        /// <param name="context">Wrapper of all program resources</param>
+        public ControlDataFile(Context context)
+        {
+            this.InitializeComponent();
+            this.Context = context;
+            this.InitializeContent();
+        }
+
+        /// <summary>
+        /// Creates new control for editing data files
+        /// </summary>
+        /// <param name="context">Wrapper of all program resources</param>
+        /// <param name="dataFile">Data file which will be edited</param>
+        public ControlDataFile(Context context, DataFile dataFile)
+        {
+
+        }
+
+        private void InitializeContent()
+        {
+
+        }
+    }
+}
